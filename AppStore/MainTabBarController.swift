ef303d67956425be699909b0ae18042a52f6b932
Zahra Alizada <elizade.zehra.ez@gmail.com>
2024-09-19 14:46:22 +0400

    Initial commit

---
+++ AppStore/MainTabBarController.swift
@@ -0,0 +1,15 @@
+//
+//  MainTabBarController.swift
+//  AppStore
+//
+//  Created by Zahra Alizada on 19.09.24.
+//
+
+import UIKit
+class MainTabBarController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .red
+    }
+}
+
+
